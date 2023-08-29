20207affdabdeb7d6d9a9ff8995ac2d8b96e4612
Rajat Sharma <115858794+rajatsharmaji@users.noreply.github.com>
2023-08-29 14:38:00 +0000

    dataTypes

---
+++ dataTypes/dataTypes.js
@@ -0,0 +1,15 @@
+//number
+//bigint
+//string => ""
+//boolean => true/false
+//undefined => value is not defined
+//null => standalone value => represent empty value
+//symbol => use for unique
+
+
+//object
+
+
+console.log(typeof null)  // object
+console.log(typeof undefined) //undefined
+console.log(typeof Symbol) //function
